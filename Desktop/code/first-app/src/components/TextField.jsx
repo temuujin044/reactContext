4016c56b8649a8a13bd790d8b83420644ec822f3
Temuujin undefined <23LP5268@G401-CC0096.local>
2024-01-15 11:45:50 +0800

    first-next

---
+++ Desktop/code/first-app/src/components/TextField.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import "@/styles/TextField.module.css";
+
+export function TextField() {}
---
+++ Desktop/code/first-app/src/pages/temuujin.js
@@ -0,0 +1,3 @@
+export default function Temuujin() {
+  return <div>Temuujin</div>;
+}
